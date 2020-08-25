6a68277cad7b3c15b0a1eead2662236d85cf4929
Mohamed Mahmoud <moh2661978@gmail.com>
2020-08-25 05:59:34 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,6 @@
-// build the nav
-
-/* Strategy:
-get the navbar list by ID & get the sections by querySelectAll to allow me loop in the return array and then pull out the data to li in the navbar list by: assign the array of sections to li and creat li in navbar
-append the sections to the li and add the newly created element and its content into the DOM */
-const menu = document.getElementById('navbar__list'); // ul inside nav tag
-const sections = document.querySelectorAll('section'); // all section tag
-// building the navbar
-function buildMenuBar() {
-    // initialize an li variable to put it later in the navigation to manipulate the loop list of sections names into it and using let for mutating the array
-    let navList = document.createElement('li');
-    // looping over all sections by the method array.forEach(element => {...});
-
-    sections.forEach((section) => {
-        // get the section id
-        const secid = section.getAttribute('id');
-        // get the sections dataset Attribute
-        const secDataNav = section.getAttribute('data-nav');
-        // looping in navList = navList + li + anchor every section name and id
-        navList += `<a href='#${secid}' class='menu__link'${secDataNav}>${secDataNav}</a>`;
-
-        // Manipulating the DOM by appending all sections titles to the navigation in a dynamic way
-        menu.innerHTML = navList;
-    });
-};
-buildMenuBar();
+// property of History interface allows web applications to explicitly set default scroll restoration behavior on history navigation
+// https://developer.mozilla.org/en-US/docs/Web/API/History/scrollRestoration
+/*window.history.scrollRestoration = 'manual';*/
 
 //Building the go to the top button:
 //https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
@@ -45,58 +22,70 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
 
-// Add class 'active' to section when near top of viewport
-//Start building the active class
+// build the nav
 
-/*As a start, we’ll listen for the 'scroll' event on the document to request the current scrollY position each time the user scrolls. I found that every 300 px the top of section get appeared
-just helper function
-*/
-document.addEventListener('scroll', () => {
-    document.documentElement.dataset.scroll = window.scrollY;
-});
+/* Strategy:
+get the navbar list by ID & get the sections by querySelectAll to allow me loop in the return array and then pull out the data to li in the navbar list by: assign the array of sections to li and creat li in navbar
+append the sections to the li and add the newly created element and its content into the DOM */
+const menu = document.getElementById('navbar__list'); // ul inside nav tag
+const sections = document.querySelectorAll('section'); // all section tag
 
-// start by return the top rect and determine the position of the section in the viewport
-const secRect = (section) => {
-    return section.getBoundingClientRect().top;
-};
-// remove the active class because of section 1 is by default has this class
-const classRemove = (section) => {
-    //remove the active class styling state
-    section.classList.remove('your-active-class');
-    //adding style to the all non active sections
-    section.style.add = ('section.your-active-class');
-};
 
-// adding the active class
-const classAdd = (isInView, section) => {
-    if (isInView) {
-        //remove the active class styling state
-        section.classList.add('your-active-class');
-        //adding style to the active section
-        section.style.add = ('section.your-active-class');
-    }
-};
+// building the navbar
+function buildMenuBar() {
+    // initialize an li variable to put it later in the navigation to manipulate the loop list of sections names into it and using let for mutating the array
+    let navList = document.createElement('li');
+    // looping over all sections by the method array.forEach(element => {...});
 
-/*
-to use the getBoundingClientRect method I will creat a variable to store back the dom rect object which will be returned to me
-getting the section position in the window
-to know an element's position after it transformed by CSS, or even still in transition animation, it is the best choice.
-*/
+    sections.forEach((section) => {
+        // get the section id
+        const secid = section.getAttribute('id');
+        // get the sections dataset Attribute
+        const secDataNav = section.getAttribute('data-nav');
+        // looping in navList = navList + li + anchor every section name and id
+        navList += `<a href='#${secid}' class='menu__link'${secDataNav}>${secDataNav}</a>`;
 
-//Manipulating the dom to set the active section function by looping over the sections
-const setBounding = () => {
-    // I Will loop on all the sections
-    sections.forEach(section => {
-        //creating a variable to hold the returning top Rect from the previous function
-        const secReturn = secRect(section);
-        // Ask if is in view to allow adding the active section function
-        isInView = () => secReturn < 300 && secReturn >= -300;
-        // remove from the previuos section
-        classRemove(section);
-        // add to the next section
-        classAdd(isInView(), section);
+        // Manipulating the DOM by appending all sections titles to the navigation in a dynamic way
+        menu.innerHTML = navList;
     });
 };
+buildMenuBar();
 
-// add eventlisner to scroll so that the function get implemented
-window.addEventListener('scroll', setBounding);
+
+// Add ActiveClass to sections
+
+/*
+*Whenever the target meets a threshold specified for the IntersectionObserver, the callback is invoked. The callback receives a list of IntersectionObserverEntry objects and the observer:
+*/
+
+// function to use for callback in the intersection observer
+
+const changeActiveSec = (entries, observer) => {
+    entries.forEach((entry) => {
+        // only do ActiveClass if the element is fully on screen
+        if (entry.isIntersecting && entry.intersectionRatio == 1) {
+            entry.target.classList.add('your-active-class');
+        } else {
+            entry.target.classList.remove('your-active-class');
+        }
+    });
+}
+/*
+*Create the intersection observer by calling its constructor and passing it a callback function to be run whenever a threshold is crossed in one direction or the other:
+*/
+
+// create the observer 
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0,
+    //A threshold of 1.0 means that when 100% of the target is visible within the element specified by the root option, the callback is invoked.
+};
+const observer = new IntersectionObserver(changeActiveSec, options);
+
+
+// target the sections to be observed
+sections.forEach((section) => {
+    observer.observe(section);
+});
+
